5c32abaef410e75e93742cff919b5592a1cfc870
Yulya <yulyazinina@ya.ru>
2022-09-01 17:28:27 +0800

    Added task_2 (Programm to choose min&max)

---
+++ home_work_1/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int min = number1;
+int max = number2;
+
+if (number1 > number2)
+{
+    min = number2; 
+    max = number1;
+}
+
+Console.Write("Меньшее число: ");
+Console.WriteLine(min);
+Console.Write("Большее число: ");
+Console.WriteLine(max);
