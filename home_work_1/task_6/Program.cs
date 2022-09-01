34e4666dd00fdced37847e55d1c3367d70e9e271
Yulya <yulyazinina@ya.ru>
2022-09-01 17:57:10 +0800

    Added task_6 (even number programm)

---
+++ home_work_1/task_6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.WriteLine(" чётное");
+}
+
+else
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.WriteLine(" нечётное");
+}
