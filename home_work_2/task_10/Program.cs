cafbcbd899ccfc96265aa86d8770634beb811df3
Yulya <yulyazinina@ya.ru>
2022-09-09 01:18:12 +0800

    Added task_10 (Second of 3-digit number programm)

---
+++ home_work_2/task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+  
+Console.WriteLine();
+Console.WriteLine(ChooseDigitSecond(number));
+  
+int ChooseDigitSecond (int num)
+{
+int digitSecond = 0;
+    if (num > 99 & num < 1000)
+    {
+        digitSecond =  num % 100 / 10;
+    }
+    else
+    {
+        Console.WriteLine("Число не подходит. Перезапустите программу");
+    }
+return digitSecond;
+}
