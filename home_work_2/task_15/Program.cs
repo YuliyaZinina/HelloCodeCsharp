2d799e32faa981ece4ad5fb3a6ab7619643c5299
Yulya <yulyazinina@ya.ru>
2022-09-09 01:32:34 +0800

    Added task_10 (Added day of week programm)

---
+++ home_work_2/task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+DefineDay(dayNumber);
+
+void DefineDay (int num)
+{
+    if (num > 5 & num <= 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else if (num < 5)
+    {
+        Console.WriteLine("Будний");
+    }
+    else
+    {
+        Console.WriteLine("Число не подходит. Перезапустите программу");
+    }
+}
