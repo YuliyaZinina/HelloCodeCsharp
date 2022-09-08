e17ce4df309b25b99d27ba4e00c6ad9663385192
Yulya <yulyazinina@ya.ru>
2022-09-09 02:02:52 +0800

    Added task_13 (Third digit programm)

---
+++ home_work_2/task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+Console.WriteLine(ChooseThirdDigit(number));
+
+char ChooseThirdDigit (string num)
+{
+    char thirdDigit = 't';
+    if (num.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+    thirdDigit = num[2];
+    }
+    return thirdDigit;
+}
