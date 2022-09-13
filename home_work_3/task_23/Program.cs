dd9a3d2db7fe135371dd5c06a53f3ce45518f159
Yulya <yulyazinina@ya.ru>
2022-09-13 17:50:11 +0800

    Added task 23. Cube table programm

---
+++ home_work_3/task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+GetCubeTable(number);
+
+void GetCubeTable (int num)
+{
+    for (int n=1; n<=num; n++)
+    {
+    int cube = (int)Math.Pow(n, 3);
+    Console.WriteLine($"{n}^3 = {cube}");
+    }
+}
