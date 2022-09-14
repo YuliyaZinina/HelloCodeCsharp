9330d76050f2826fc620a89276e58610fa6e20d3
Yulya <yulyazinina@ya.ru>
2022-09-14 17:02:58 +0800

    Added task 29. Array[8] programm

---
+++ home_work_4/task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите элементы массива, состоящего из чисел: ");
+double[] newArray = GetArray();
+Console.Write("Вы задали массив: ");
+PrintArray(newArray);
+
+double[] GetArray()
+{
+    double[]array = new double[8];
+    for (int i = 1; i <= 8; i++)
+    {
+        int index = i - 1;
+        Console.Write($"Введите число {i}: ");
+        array[index] = Convert.ToDouble(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(double[]array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]} ");
+    }
+    Console.WriteLine();
+}
