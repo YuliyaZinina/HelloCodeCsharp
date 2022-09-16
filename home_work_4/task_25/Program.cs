fc39a1ab24afa9cec1d08a2a5ebe38a5f1a7f421
Yulya <yulyazinina@ya.ru>
2022-09-16 17:55:28 +0800

    Added task 25. Cycle power

---
+++ home_work_4/task_25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//           и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{numberA}^{numberB} = {GetPower(numberA, numberB)}");
+
+int GetPower(int num1, int num2)
+{
+    int pow = Math.Abs(num2);
+    int result = 1;
+    for (int i=1; i<=pow; i++)
+    {
+      result = result * num1;
+    }
+    return result;
+}
---
+++ home_work_4/task_27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+//            и выдаёт сумму цифр в числе.
+
+Console.WriteLine('Введите число');
+int number = Console.WriteLine();
+
+int GetSummDigits (num)
+{
+   summDigitsb
+}
