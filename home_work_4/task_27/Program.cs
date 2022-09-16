399a03c3d783a43f07eddf49048aac2470f1ec6e
Yulya <yulyazinina@ya.ru>
2022-09-16 18:11:33 +0800

    Added task 27. Sum of digits

--- home_work_4/task_27/Program.cs
+++ home_work_4/task_27/Program.cs
@@ -1,10 +1,32 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число
 //            и выдаёт сумму цифр в числе.
 
-Console.WriteLine('Введите число');
-int number = Console.WriteLine();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} = {GetSumDigits(number)}");
 
-int GetSummDigits (num)
+int GetCount(int num)
 {
-   summDigitsb
+   int count = 1;
+   while (num >= 10)
+   {
+      num = num / 10;
+      count++;
+   }
+   return count;
+}
+
+int GetSumDigits (int num)
+{
+   int digit = 0;
+   int count = GetCount(num);
+   int sumDigits = 0;
+
+   while (num > 0)
+   {
+      digit = num % 10;
+      num = num / 10;
+      sumDigits = sumDigits + digit;
+   }
+   return sumDigits;
 }
