50e0219183386e7b793f99f6888a3d3c48c9c5bf
Yulya <yulyazinina@ya.ru>
2022-09-28 12:42:59 +0800

    Added task 58. Get product of matrix

---
+++ home_work_8/task_58/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] D = new int[,]
+{
+    {2, 4},
+    {3, 2}
+};
+
+int[,] E = new int[,]
+{
+    {3, 4},
+    {3, 3}
+};
+
+int[,] F = GetProductMatrix (D, E);
+
+int[,] GetProductMatrix (int[,] A, int[,] B)
+{
+    int[,] C = new int[A.GetLength(0), B.GetLength(1)];
+    for (int i = 0; i < A.GetLength(0); i++)
+    {
+        for (int j = 0; j < B.GetLength(1); j++)
+        {
+            C[i, j] = A[i, 0] * B[0, j] 
+                    + A[i, 1] * B[1, j];
+            Console.WriteLine(C[i, j]);
+        }
+    }
+    return C;
+}
