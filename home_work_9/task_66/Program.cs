5dff0a2774c7835bfd9b41f3fa6cdaed3c3c996d
Yulya <yulyazinina@ya.ru>
2022-09-30 17:44:38 +0800

    Added task 64. Recursion sum

---
+++ home_work_9/task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿int numberM = 1;
+int numberN = 15;
+
+if (numberM < numberN)
+{
+    Console.WriteLine (Func(numberM, numberN));
+}
+else
+{
+    Console.WriteLine (Func(numberN, numberM));
+}
+
+
+int Func(int numFrom, int numTo)
+{ 
+    if (numFrom > numTo)
+    {
+      return 0;
+    }
+    else
+    {
+      return numFrom+ Func(numFrom+1, numTo);
+    }
+}
