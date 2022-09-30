01e4ff7a38f7716ac9c0a62af414a4e74fc0079c
Yulya <yulyazinina@ya.ru>
2022-09-30 18:10:27 +0800

    Added task 68. Akkerman function

---
+++ home_work_9/task_68/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberM = 3;
+int numberN = 2;
+
+Console.WriteLine(Akkerman(numberM, numberN));
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akkerman(m-1, 1);
+    }
+    else
+    {
+        return Akkerman(m-1, Akkerman(m, n-1));
+    }
+}
